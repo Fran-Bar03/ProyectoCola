750170af52766d2b5e71f50eb7c78453d18975c7
Fran-Bar03 <papihachi_vlk@outlook.com>
2024-11-06 23:27:08 -0700

    finalizado

---
+++ Cola.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCola
+{
+    internal class Cola
+    {
+        private Nodo inicio;
+        private int count = 0;
+        private int MAX;
+
+        public Cola(int max)
+        {
+            MAX = max;
+            inicio = null;
+        }
+
+        private bool underflow()
+        {
+            if (inicio == null)
+                return true;
+            else
+                return false;
+        }
+
+        private bool overflow()
+        {
+            if (count == MAX)
+                return true;
+            else
+                return false;
+        }
+
+        public void Print()
+        {
+            if (underflow())
+            {
+                Console.WriteLine("La cola esta vacia");
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+            else
+            {
+                Nodo actual = inicio;
+
+                while (actual != null)
+                {
+                    Console.Write($"--- {actual.Valor} ---> ");
+                    actual = actual.Sig;
+
+                }
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+        }
+
+        public int Count()
+        {
+            return count;    
+        }
+        
+        public bool Insert(int num)
+        {
+            if (overflow())
+            {
+                Console.WriteLine("La cola esta llena");
+                Console.WriteLine("");
+                return false;
+            }
+
+            Nodo nuevo = new Nodo(num);
+            
+            if (underflow())
+            {
+                inicio = nuevo;
+            } 
+            
+            else
+            {
+                Nodo actual = inicio;
+                while(actual.Sig != null)
+                {
+                    actual = actual.Sig;
+                }
+                actual.Sig = nuevo;
+            }
+            count++;
+            return true;
+
+            //regresa true al insertar exitosamente
+            //false si la cola esta llena y no se pudo insertar
+        }
+        
+        public int Extract()
+        {
+            if (underflow())
+            {
+                Console.WriteLine("No pudo ser extraido, cola vacia");
+                return -1;
+            }
+
+            int ValorExtraido = inicio.Valor;
+            inicio = inicio.Sig;
+            count--;
+
+            Console.WriteLine($"El numero sacado de la cola : {ValorExtraido}");
+
+            return ValorExtraido;
+            
+            //regresa el valor extraido de la cola
+            //si esta vacia regresa -1 porque no pudo extraer
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cola micola = null;
+            int op = 0;
+            do
+            {
+                Console.WriteLine("|-----------------------------------|");
+                Console.WriteLine("|--------- PROYECTO COLA ----------|");
+                Console.WriteLine("|-----------------------------------|");
+                Console.WriteLine("");
+                Console.WriteLine("1.Insertar tamaño");
+                Console.WriteLine("2.Insert");
+                Console.WriteLine("3.Extract");
+                Console.WriteLine("4.Count");
+                Console.WriteLine("5.Imprimir");
+                Console.WriteLine("6.Salir");
+                Console.WriteLine("");
+                Console.WriteLine("Seleccionar opcion =>");
+                op = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("");
+                Console.WriteLine("");
+
+
+                switch (op)
+                {
+                    case 1:
+                        Console.WriteLine("Dame el numero a insertar");
+                        int max = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        if (max > 0)
+                        {
+                            micola = new Cola(max);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Introduzca un tamaño de cola mayor");
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                        }
+                        break;
+
+
+                    case 2:
+                        if (micola == null)
+                        {
+                            Console.WriteLine("Introduzca un tamaño de cola primero");
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ingrese el numero a insertar");
+                            int num = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                            micola.Insert(num);
+                        }
+                        break;
+
+
+                    case 3:
+                        if (micola == null)
+                        {
+                            Console.WriteLine("Introduzca un tamaño de cola primero");
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                        }
+                        else
+                        {
+                            micola.Extract();
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                        }
+                        break;
+
+
+                    case 4:
+                        if (micola == null)
+                        {
+                           Console.WriteLine("Introduzca un tamaño de cola primero");
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                        }
+
+                        else if(micola.Count() == 0) 
+                        {
+                            Console.WriteLine("La cola está vacía");
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"La cola tiene {micola.Count()} nodos");
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                        }
+                        break;
+
+                    case 5:
+                        if (micola == null)
+                        {
+                            Console.WriteLine("Introduzca un tamaño de cola primero");
+                            Console.WriteLine("");
+                            Console.WriteLine("");
+                        }
+                        else
+                        {
+                            micola.Print();
+                        }
+                        break;
+
+
+                    case 6:
+                        Console.WriteLine("Saliendo.....");
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        break;
+
+                    default:
+                        Console.WriteLine("Introduzca una opcion valida");
+                        Console.WriteLine("");
+                        Console.WriteLine("");
+                        break;
+                }
+            } while (op != 6);
+        }
+    }
+}
